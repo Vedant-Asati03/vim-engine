f4e4ebfd1c967d440d6f7959f0b295d9523ce6e9
vedant-asati03 <vedant.asati03@gmail.com>
2025-12-03 11:24:47 +0530

    feat: Organize buffer module imports and update __all__ exports for clarity

--- src/vim_engine/buffer/__init__.py
+++ src/vim_engine/buffer/__init__.py
@@ -1,1 +1,26 @@
 """Buffer abstractions and undo/redo data structures."""
+
+from .buffer import Buffer, BufferDelta, BufferView, Transaction
+from .document import BufferDocument
+from .registers import RegisterBank, RegisterValue
+from .state import BufferState
+from .sync import BufferMirror, BufferSync, BufferValidationError
+from .undo import UndoEntry, UndoTimeline
+from .validation import ensure_cursor
+
+__all__ = [
+    "BufferDocument",
+    "BufferState",
+    "RegisterBank",
+    "RegisterValue",
+    "UndoTimeline",
+    "UndoEntry",
+    "Buffer",
+    "BufferDelta",
+    "BufferView",
+    "Transaction",
+    "BufferMirror",
+    "BufferSync",
+    "BufferValidationError",
+    "ensure_cursor",
+]
